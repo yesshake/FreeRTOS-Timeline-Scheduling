7fa3539c5025953e507fa13bfd86dd302f5f8154
Yesshake <yisakzinabu@gmail.com>
2026-05-19 14:21:15 +0200

    Add dummy tasks for dubug

--- Demo/FreeRTOSConfig.h
+++ Demo/FreeRTOSConfig.h
@@ -1,90 +1,90 @@
 #ifndef FREERTOS_CONFIG_H
-#define FREERTOS_CONFIG_H
+	#define FREERTOS_CONFIG_H
 
-#define configUSE_TRACE_FACILITY        1
-#define configGENERATE_RUN_TIME_STATS   0
+	#define configUSE_TRACE_FACILITY        1
+	#define configGENERATE_RUN_TIME_STATS   0
 
-/* Timeline Scheduler Configuration */
-#define configUSE_TIMELINE_SCHEDULER    1
-#define configTIMELINE_TASK_COUNT 4
-#define configTIMELINE_MAJOR_FRAME_TICKS 30
-#define configTIMELINE_MINOR_FRAME_TICKS 15
-
-
-/* Enable Hooks for Deadline/Kill Monitoring */
-#define configUSE_TIMELINE_DEADLINE_HOOK 0
-#define configTIMELINE_SUBFRAMES_PER_CYCLE (configTIMELINE_MAJOR_FRAME_TICKS / configTIMELINE_MINOR_FRAME_TICKS)
+	/* Timeline Scheduler Configuration */
+	#define configUSE_TIMELINE_SCHEDULER    1
+	#define configTIMELINE_TASK_COUNT 4
+	#define configTIMELINE_MAJOR_FRAME_TICKS 30
+	#define configTIMELINE_MINOR_FRAME_TICKS 6
 
 
-#define configUSE_TIME_SLICING					0
-#define configUSE_PREEMPTION            		1
-#define configUSE_IDLE_HOOK             		0
-#define configUSE_TICK_HOOK             		0
-#define configCPU_CLOCK_HZ              		( ( unsigned long ) 25000000 )
-#define configTICK_RATE_HZ              		( ( TickType_t ) 1000 )
-#define configMINIMAL_STACK_SIZE        		( ( unsigned short ) 80 )
-#define configMAX_TASK_STACK_SIZE       		( ( unsigned short ) 256 )
-#define configTOTAL_HEAP_SIZE           		( ( size_t ) ( 60 * 1024 ) )
-#define configMAX_TASK_NAME_LEN         		( 16 )
-#define configUSE_16_BIT_TICKS                   0
-#define configIDLE_SHOULD_YIELD                  0
-#define configUSE_CO_ROUTINES                    0
-#define configUSE_MUTEXES                        1
-#define configUSE_RECURSIVE_MUTEXES              1
-#define configCHECK_FOR_STACK_OVERFLOW           0
-#define configUSE_MALLOC_FAILED_HOOK             0
-#define configUSE_QUEUE_SETS                     1
-#define configUSE_COUNTING_SEMAPHORES            1
+	/* Enable Hooks for Deadline/Kill Monitoring */
+	#define configUSE_TIMELINE_DEADLINE_HOOK 0
+	#define configTIMELINE_SUBFRAMES_PER_CYCLE (configTIMELINE_MAJOR_FRAME_TICKS / configTIMELINE_MINOR_FRAME_TICKS)
 
-#define configMAX_PRIORITIES                     ( 3UL )
-#define configMAX_CO_ROUTINE_PRIORITIES          ( 2 )
-#define configQUEUE_REGISTRY_SIZE                10
-#define configSUPPORT_STATIC_ALLOCATION          1
-#define configKERNEL_PROVIDED_STATIC_MEMORY      1
 
-/* Timer related defines (unused but required by some ports) */
-#define configUSE_TIMERS                         0
-#define configTIMER_TASK_PRIORITY                ( configMAX_PRIORITIES - 1 )
-#define configTIMER_QUEUE_LENGTH                 20
-#define configTIMER_TASK_STACK_DEPTH             ( configMINIMAL_STACK_SIZE * 2 )
+	#define configUSE_TIME_SLICING					0
+	#define configUSE_PREEMPTION            		1
+	#define configUSE_IDLE_HOOK             		0
+	#define configUSE_TICK_HOOK             		0
+	#define configCPU_CLOCK_HZ              		( ( unsigned long ) 25000000 )
+	#define configTICK_RATE_HZ              		( ( TickType_t ) 1000 )
+	#define configMINIMAL_STACK_SIZE        		( ( unsigned short ) 80 )
+	#define configMAX_TASK_STACK_SIZE       		( ( unsigned short ) 256 )
+	#define configTOTAL_HEAP_SIZE           		( ( size_t ) ( 60 * 1024 ) )
+	#define configMAX_TASK_NAME_LEN         		( 16 )
+	#define configUSE_16_BIT_TICKS                   0
+	#define configIDLE_SHOULD_YIELD                  0
+	#define configUSE_CO_ROUTINES                    0
+	#define configUSE_MUTEXES                        1
+	#define configUSE_RECURSIVE_MUTEXES              1
+	#define configCHECK_FOR_STACK_OVERFLOW           0
+	#define configUSE_MALLOC_FAILED_HOOK             0
+	#define configUSE_QUEUE_SETS                     1
+	#define configUSE_COUNTING_SEMAPHORES            1
 
-#define configUSE_TASK_NOTIFICATIONS             1
-#define configTASK_NOTIFICATION_ARRAY_ENTRIES    3
+	#define configMAX_PRIORITIES                     ( 3UL )
+	#define configMAX_CO_ROUTINE_PRIORITIES          ( 2 )
+	#define configQUEUE_REGISTRY_SIZE                10
+	#define configSUPPORT_STATIC_ALLOCATION          1
+	#define configKERNEL_PROVIDED_STATIC_MEMORY      1
 
-/* API Includes */
-#define INCLUDE_vTaskPrioritySet                  1
-#define INCLUDE_uxTaskPriorityGet                 1
-#define INCLUDE_vTaskDelete                       1
-#define INCLUDE_vTaskCleanUpResources             0
-#define INCLUDE_vTaskSuspend                      1
-#define INCLUDE_vTaskDelayUntil                   1
-#define INCLUDE_vTaskDelay                        1
-#define INCLUDE_uxTaskGetStackHighWaterMark       1
-#define INCLUDE_xTaskGetSchedulerState            1
-#define INCLUDE_xTimerGetTimerDaemonTaskHandle    1
-#define INCLUDE_xTaskGetIdleTaskHandle            1
-#define INCLUDE_xSemaphoreGetMutexHolder          1
-#define INCLUDE_eTaskGetState                     1
-#define INCLUDE_xTimerPendFunctionCall            1
-#define INCLUDE_xTaskAbortDelay                   1
-#define INCLUDE_xTaskGetHandle                    1
+	/* Timer related defines (unused but required by some ports) */
+	#define configUSE_TIMERS                         0
+	#define configTIMER_TASK_PRIORITY                ( configMAX_PRIORITIES - 1 )
+	#define configTIMER_QUEUE_LENGTH                 20
+	#define configTIMER_TASK_STACK_DEPTH             ( configMINIMAL_STACK_SIZE * 2 )
 
-#define configUSE_STATS_FORMATTING_FUNCTIONS      0
+	#define configUSE_TASK_NOTIFICATIONS             1
+	#define configTASK_NOTIFICATION_ARRAY_ENTRIES    3
 
-#define configKERNEL_INTERRUPT_PRIORITY           ( 255 )
-#ifndef _IASMARM_
-	#define configASSERT( x ) if( ( x ) == 0 ) while(1);
-#endif
+	/* API Includes */
+	#define INCLUDE_vTaskPrioritySet                  1
+	#define INCLUDE_uxTaskPriorityGet                 1
+	#define INCLUDE_vTaskDelete                       1
+	#define INCLUDE_vTaskCleanUpResources             0
+	#define INCLUDE_vTaskSuspend                      1
+	#define INCLUDE_vTaskDelayUntil                   1
+	#define INCLUDE_vTaskDelay                        1
+	#define INCLUDE_uxTaskGetStackHighWaterMark       1
+	#define INCLUDE_xTaskGetSchedulerState            1
+	#define INCLUDE_xTimerGetTimerDaemonTaskHandle    1
+	#define INCLUDE_xTaskGetIdleTaskHandle            1
+	#define INCLUDE_xSemaphoreGetMutexHolder          1
+	#define INCLUDE_eTaskGetState                     1
+	#define INCLUDE_xTimerPendFunctionCall            1
+	#define INCLUDE_xTaskAbortDelay                   1
+	#define INCLUDE_xTaskGetHandle                    1
 
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY             ( 4 )
-#define configUSE_PORT_OPTIMISED_TASK_SELECTION          1
-#define configRUN_ADDITIONAL_TESTS                       1
-#define configSTREAM_BUFFER_TRIGGER_LEVEL_TEST_MARGIN    4
+	#define configUSE_STATS_FORMATTING_FUNCTIONS      0
 
-#define intqHIGHER_PRIORITY      ( configMAX_PRIORITIES - 5 )
-#define bktPRIMARY_PRIORITY      ( configMAX_PRIORITIES - 3 )
-#define bktSECONDARY_PRIORITY    ( configMAX_PRIORITIES - 4 )
+	#define configKERNEL_INTERRUPT_PRIORITY           ( 255 )
+	#ifndef _IASMARM_
+		#define configASSERT( x ) if( ( x ) == 0 ) while(1);
+	#endif
 
-#define configENABLE_BACKWARD_COMPATIBILITY 0
+	#define configMAX_SYSCALL_INTERRUPT_PRIORITY             ( 4 )
+	#define configUSE_PORT_OPTIMISED_TASK_SELECTION          1
+	#define configRUN_ADDITIONAL_TESTS                       1
+	#define configSTREAM_BUFFER_TRIGGER_LEVEL_TEST_MARGIN    4
+
+	#define intqHIGHER_PRIORITY      ( configMAX_PRIORITIES - 5 )
+	#define bktPRIMARY_PRIORITY      ( configMAX_PRIORITIES - 3 )
+	#define bktSECONDARY_PRIORITY    ( configMAX_PRIORITIES - 4 )
+
+	#define configENABLE_BACKWARD_COMPATIBILITY 0
 
 #endif /* FREERTOS_CONFIG_H */
--- Demo/main.c
+++ Demo/main.c
@@ -1,11 +1,22 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#include "FreeRTOSConfig.h"
+#include "timelinescheduler.h"
+
+
 #include "uart.h"
 
-#define mainTASK_PRIORITY    ( tskIDLE_PRIORITY + 2 )
+#define TASK_STACK_SIZE 128
 
-void vTaskFunction(void *pvParameters);
+void hrt1func(void *param);
+void hrt2func(void *param);
+void hrt3func(void *param);
+void srt1func(void *param);
+
+/* Hooks need to be declared if they are used */
+void vApplicationTimelineDeadlineMissedHook( TaskHandle_t xTask );
+void vApplicationTimelineTaskKilledHook( TaskHandle_t xTask );
 
 int main(int argc, char **argv){
 
@@ -14,20 +25,24 @@ int main(int argc, char **argv){
 
     UART_init();
 
-	xTaskCreate(
-		// Function which implements the task
-		vTaskFunction,
-		// Name of the task (debug purposes, not used by the kernel)
-		"Task1",
-		// Stack to allocate to the task
-		configMINIMAL_STACK_SIZE,
-		// Parameter passed to the task. Not needed for Hello World example
-		NULL,
-		// Priority assigned to the task
-		mainTASK_PRIORITY,
-		// Task handle. Not required
-		NULL
-	);
+    // const char       *pcTaskName;
+    // TaskFunction_t    pxTaskCode;
+    // TaskType_t        xType;
+    // TickType_t        xStartTime;
+    // TickType_t        xEndTime;
+    // uint32_t          ulSubframeId;
+    // uint16_t          usStackDepth;
+
+
+    TimelineTaskConfig_t tasks[configTIMELINE_TASK_COUNT] = 
+    {
+        {"hrt1", hrt3func, TASK_HARD_RT, 0, 3, 0, TASK_STACK_SIZE},
+        {"srt1", hrt1func, TASK_HARD_RT, 8, 11, 1, TASK_STACK_SIZE},
+        {"srt2", hrt2func, TASK_SOFT_RT, -1, -1, -1, TASK_STACK_SIZE},
+        {"srt3", hrt1func, TASK_SOFT_RT, 20, 25, 4, TASK_STACK_SIZE}
+    };
+
+    vConfigureScheduler(tasks);
 
 	// Give control to the scheduler
 	vTaskStartScheduler();
@@ -36,18 +51,59 @@ int main(int argc, char **argv){
     for( ; ; );
 }
 
-/* Task Function */
-void vTaskFunction(void *pvParameters) {
+void hrt1func(void *param){
+    (void) param;
+    for(int i = 0; i < 10000000000; i++){
+        // UART_puts("hrt1");
+        for(int j = 0; j < 1000000000; j++){
+        // UART_puts("hrt1");
+        float a = 95537, b=8476.9;
+        volatile float c = 0;
+        c = a / b;
 
-	// Avoid warning about unused pvParameters
-	(void) pvParameters;
-
-    for (;;) {
-
-        // Task code: print a message
-        UART_printf("Hello, World!\n");
-
-        // Delay for 1 second
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        }
     }
+    // UART_puts("\n");
+    vTaskSuspend(NULL);
+}
+
+void hrt2func(void *param){
+    (void) param;
+        for(int i = 0; i < 10000000000; i++){
+        // UART_puts("hrt1");
+        for(int j = 0; j < 1000000000; j++){
+        // UART_puts("hrt1");
+        float a = 95537, b=8476.9;
+        volatile float c = 0;
+        c = a / b;
+
+        }
+    }    
+    // UART_puts("\n");
+    vTaskSuspend(NULL);
+}
+
+void hrt3func(void *param){
+    (void) param;
+    for(int i = 0; i < 1000; i++){
+        // UART_puts("hrt3");
+    }
+    // UART_puts("\n");
+    vTaskSuspend(NULL);
+}
+
+void srt1func(void *param){
+    (void) param;
+    /* Use volatile to prevent the compiler from optimizing the loop away */
+    volatile int dummy = 0;
+    for(int i = 0; i < 1000; i++){
+        for(int j = 0; j < 100; j++){
+            float a = 95537, b = 8476.9;
+            volatile float c = 0;
+            c = a / b;
+            dummy += (int)c;
+        }
+    }
+    (void) dummy;
+    vTaskSuspend(NULL);
 }
